66c303359874c4c814ce329605496696bf23aa39
Hamza Yoğurtcuoğlu <hamzayogurtcuoglu@gmail.com>
2020-01-23 01:03:04 +0300

    Update cameraRecord.py

--- cameraRecord.py
+++ cameraRecord.py
@@ -6,9 +6,7 @@ cap = cv2.VideoCapture(0)
 if (cap.isOpened() == False): 
   print("Unable to read camera feed")
  
-# Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# We convert the resolutions from float to integer.
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+
 out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
  
 while(True):
@@ -16,23 +14,17 @@ while(True):
  
   if ret == True: 
      
-    # Write the frame into the file 'output.avi'
     out.write(frame)
  
-    # Display the resulting frame    
     cv2.imshow('frame',frame)
  
-    # Press Q on keyboard to stop recording
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
  
-  # Break the loop
   else:
     break 
  
-# When everything done, release the video capture and video write objects
 cap.release()
 out.release()
  
-# Closes all the frames
 cv2.destroyAllWindows() 	
